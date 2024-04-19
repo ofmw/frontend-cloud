69f328b853390b9aaec43bd1f171d1d201aea796
ofmw <ofmw91@gmail.com>
2024-04-19 16:47:22 +0900

    Edit Protocol

--- src/services/EmployeeService.js
+++ src/services/EmployeeService.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-const EMPLOYEE_API_BASE_URL = "http://api.ofmw.site/api/v1/employees";
+const EMPLOYEE_API_BASE_URL = "https://api.ofmw.site/api/v1/employees";
 
 class EmployeeService {
 
